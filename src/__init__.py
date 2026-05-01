a971af68e3de3dec4ce2a225e577a0ab2e5e202e
animeshn72 <animeshn72@gmail.com>
2026-05-02 00:34:21 +0530

    changes for packaging

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from src.routeFinder import RouteFinder
+from src.distanceCalc import DistanceCalculator
--- src/distanceCalc/distance_calculator.py
+++ src/distanceCalc/distance_calculator.py
@@ -1,16 +1,14 @@
 import math
 
 class DistanceCalculator:
-    """Handles distance calculations between each node in the graph."""
+    # Handles distance calculations between each node in the graph
     
     # assumption for calculating distance between 2 locations using latitude and longitude
     EARTH_RADIUS_KM = 6371.0
 
     @staticmethod
     def calc_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
-        """
-        Calculates the great-circle distance between two points on Earth.
-        """
+        # Calculates the great-circle distance between two points on Earth
         # Convert decimal degrees to radians
         d_lat = math.radians(lat2 - lat1)
         d_lon = math.radians(lon2 - lon1)
